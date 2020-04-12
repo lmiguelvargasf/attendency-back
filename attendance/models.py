399f0a26cc78a915dcbcd22bf289dce605db6fad
M <m@mathsistor.com>
2020-04-12 12:49:07 -0500

    Redefine model Member and regenerate migration

--- attendance/models.py
+++ attendance/models.py
@@ -6,7 +6,7 @@ class Member(TimeStampedModel):
     first_name = models.CharField(max_length=64)
     middle_name = models.CharField(max_length=64, blank=True, default='')
     last_name = models.CharField(max_length=64)
-    preferred_name = models.CharField(max_length=64)
+    preferred_name = models.CharField(max_length=64, blank=True, default='')
     email = models.EmailField()
 
     def __str__(self):
