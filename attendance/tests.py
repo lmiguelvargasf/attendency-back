9f597892617f5171b9a88b33938c0a4ed1f9929a
M <m@mathsistor.com>
2020-04-12 13:44:29 -0500

    Improve docstring for str Member tests

--- attendance/tests.py
+++ attendance/tests.py
@@ -14,16 +14,21 @@ def member():
 @pytest.mark.django_db
 def test_str_member_when_no_preferred_name(member):
     """Test that concatenation of first and last names is
-    returned as string representation when no preferred name
-    is provided"""
+    returned as string representation of Member instance when no
+    preferred name is provided"""
     assert str(member) == 'John Doe'
 
 
 @pytest.mark.django_db
 def test_str_member_when_preferred_name(member):
     """Test that preferred name is returned as string representation
-    when preferred name is provided"""
+    of Member instance when preferred name is provided"""
     member.preferred_name = 'JD'
     member.save()
 
     assert str(member) == 'JD'
+
+@pytest.mark.django_db
+def test_str_project():
+    """Test that title is returned as string representation of Member model"""
+    pass
