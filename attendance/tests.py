5cceb987e1b8a448987f30f951714973fab3c4b2
M <m@mathsistor.com>
2020-04-12 13:00:17 -0500

    Implement tests for str representation of Member

--- attendance/tests.py
+++ attendance/tests.py
@@ -1,3 +1,31 @@
-from django.test import TestCase
+import pytest
 
-# Create your tests here.
+from .models import Member
+
+
+@pytest.mark.django_db
+def test_str_member_when_no_preferred_name():
+    """Test that concatenation of first and last names is
+    returned as string representation when no preferred name
+    is provided"""
+    member = Member.objects.create(
+        first_name='John',
+        last_name='Doe',
+        email='john.doe@example.com',
+    )
+
+    assert str(member) == 'John Doe'
+
+
+@pytest.mark.django_db
+def test_str_member_when_preferred_name():
+    """Test that preferred name is returned as string representation
+    when preferred name is provided"""
+    member = Member.objects.create(
+        first_name='John',
+        last_name='Doe',
+        preferred_name='JD',
+        email='john.doe@example.com',
+    )
+
+    assert str(member) == 'JD'
