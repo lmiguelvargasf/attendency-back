f5985f0e4bc9062334c57e7174c1f34a2d1986ee
M <m@mathsistor.com>
2020-04-12 13:32:00 -0500

    Register model Project in Django admin (custom list_display)

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 
-from .models import Member
+from .models import Member, Project
 
 
 admin.site.register(Member)
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('title', 'start_date', 'description')
