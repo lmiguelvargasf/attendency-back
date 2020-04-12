c355ab5ce66f9c0e566083486f5a23c1cbd9d267
M <m@mathsistor.com>
2020-04-12 13:06:56 -0500

    Refactor: create fixture to avoid repetition

--- attendance/tests.py
+++ attendance/tests.py
@@ -2,30 +2,28 @@ import pytest
 
 from .models import Member
 
-
-@pytest.mark.django_db
-def test_str_member_when_no_preferred_name():
-    """Test that concatenation of first and last names is
-    returned as string representation when no preferred name
-    is provided"""
-    member = Member.objects.create(
+@pytest.fixture
+def member():
+    return Member.objects.create(
         first_name='John',
         last_name='Doe',
         email='john.doe@example.com',
     )
 
+
+@pytest.mark.django_db
+def test_str_member_when_no_preferred_name(member):
+    """Test that concatenation of first and last names is
+    returned as string representation when no preferred name
+    is provided"""
     assert str(member) == 'John Doe'
 
 
 @pytest.mark.django_db
-def test_str_member_when_preferred_name():
+def test_str_member_when_preferred_name(member):
     """Test that preferred name is returned as string representation
     when preferred name is provided"""
-    member = Member.objects.create(
-        first_name='John',
-        last_name='Doe',
-        preferred_name='JD',
-        email='john.doe@example.com',
-    )
+    member.preferred_name = 'JD'
+    member.save()
 
     assert str(member) == 'JD'
