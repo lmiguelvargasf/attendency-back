5b7eb30fc940bed19f362a4870f7e0546816f9ea
M <m@mathsistor.com>
2020-04-12 21:06:12 -0500

    Register model Meeting in Django admin

--- attendance/admin.py
+++ attendance/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Member, Project
+from .models import Member, Project, Meeting
 
 
 admin.site.register(Member)
@@ -9,3 +9,8 @@ admin.site.register(Member)
 @admin.register(Project)
 class ProjectAdmin(admin.ModelAdmin):
     list_display = ('title', 'start_date', 'description', 'team')
+
+
+@admin.register(Meeting)
+class MeetingAdmin(admin.ModelAdmin):
+    list_display = ('project', 'date', 'time')
