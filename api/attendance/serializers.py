edbd8b8b782f82a0b9063727bda0c4f5f9806902
M <m@mathsistor.com>
2020-04-15 00:06:24 -0500

    Implement viewset for Project

--- api/attendance/serializers.py
+++ api/attendance/serializers.py
@@ -1,8 +1,8 @@
-from rest_framework import serializers
+from rest_framework.serializers import ModelSerializer
 
 from attendance.models import Project
 
-class ProjectSerializer(serializers.ModelSerializer):
+class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
         fields = '__all__'
---
+++ api/attendance/views.py
@@ -0,0 +1,9 @@
+from rest_framework.viewsets import ModelViewSet
+
+from attendance.models import Project
+from .serializers import ProjectSerializer
+
+
+class ProjectViewSet(ModelViewSet):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
