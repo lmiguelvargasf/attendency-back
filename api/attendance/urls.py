dd1b6c3fab6c59d8d1dfb1e93646057b2760835b
M <m@mathsistor.com>
2020-04-16 18:11:31 -0500

    Register MemberViewSet

--- api/attendance/urls.py
+++ api/attendance/urls.py
@@ -1,9 +1,10 @@
 from rest_framework.routers import DefaultRouter
 
-from .views import ProjectViewSet
+from .views import ProjectViewSet, MemberViewSet
 
 
 router = DefaultRouter()
 router.register(r'projects', ProjectViewSet)
+router.register(r'members', MemberViewSet)
 
 urlpatterns = router.urls
