4ca184ded6dc47ad4b7c79f21c09e4f18edc13b3
M <m@mathsistor.com>
2020-04-17 00:30:28 -0500

    Create base class BaseReactSerializer

--- api/attendance/serializers.py
+++ api/attendance/serializers.py
@@ -3,24 +3,23 @@ from rest_framework import serializers
 from attendance.models import Project, Member, Meeting
 
 
-class ProjectTableSerializer(serializers.ModelSerializer):
+class BaseReactSerializer(serializers.ModelSerializer):
     key = serializers.IntegerField(source='id')
 
+
+class ProjectTableSerializer(BaseReactSerializer):
     class Meta:
         model = Project
         fields = ('key', 'title', 'start_date', 'description', 'team')
 
 
-class MemberTableSerializer(serializers.ModelSerializer):
-    key = serializers.IntegerField(source='id')
-
+class MemberTableSerializer(BaseReactSerializer):
     class Meta:
         model = Member
         fields = ('key', 'first_name', 'last_name', 'email')
 
 
-class MeetingTableSerializer(serializers.ModelSerializer):
-    key = serializers.IntegerField(source='id')
+class MeetingTableSerializer(BaseReactSerializer):
     project = serializers.CharField()
 
     class Meta:
