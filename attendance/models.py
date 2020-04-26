6ea1e78dd840b0785c0ff58b0e8f2b146ad2fa77
M <m@mathsistor.com>
2020-04-25 20:15:28 -0500

    Add field observations to Meeting

--- attendance/models.py
+++ attendance/models.py
@@ -42,6 +42,7 @@ class Meeting(TimeStampedModel):
         related_query_name="meeting",
     )
     date_time = models.DateTimeField()
+    observations = models.TextField(blank=True)
 
     @property
     def __localtime(self):
