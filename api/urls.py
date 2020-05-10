b003d7652e2bd44b7c0f74d25a3f765fe0d5613e
M <m@mathsistor.com>
2020-05-10 10:38:44 -0500

    Move token-auth url to api urls

--- api/urls.py
+++ api/urls.py
@@ -1,3 +1,9 @@
+from django.urls import path
+from rest_framework_jwt.views import obtain_jwt_token
+
 from .attendance.urls import urlpatterns as attendance_urls
 
 urlpatterns = attendance_urls
+urlpatterns += [
+    path('token-auth/', obtain_jwt_token),
+]
